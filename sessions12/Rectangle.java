862d67886e0f0b6db5f2af95b2bb487daafeb4d0
ZeusPhucNguyen <73330508+ZeusPhucNguyen@users.noreply.github.com>
2021-04-24 21:22:13 +0700

    Add files via upload

---
+++ sessions12/Rectangle.java
@@ -0,0 +1,12 @@
+package sessions12;
+
+public class Rectangle extends Shape {
+
+    float perimetter;
+    float length=10;
+    @Override
+    void calculate (float width){
+        perimetter=2*(length+width);
+        System.out.println("Perimeter of the Rectangle is:"+perimetter);
+    }
+}
---
+++ sessions12/Vehicle.java
@@ -0,0 +1,15 @@
+package sessions12;
+
+public class Vehicle {
+    public String vehicleNo;
+    public String vehicleName;
+    public int wheels;
+    /**
+     * Accelerates the vehicle
+     *
+     * @return void
+     */
+    public void accelerate(int speed) {
+        System.out.println("Accelerating at:"+speed+ "kmph");
+    }
+}
